4163fba513b9611a08723a85828579cb83eeb19f
Lachy Richards <lachyrichards@Lachys-MacBook-Pro.local>
2024-02-19 11:26:00 +1100

    animations

--- app/components/TagsFilter.tsx
+++ app/components/TagsFilter.tsx
@@ -1,3 +1,66 @@
+// "use client";
+
+// import React, { useState } from "react";
+// import { Tag } from "@/types";
+
+// interface TagsFilterProps {
+//   tags: Tag[];
+//   onSelectTag: (selectedTagSlug: string | null) => void;
+// }
+
+// const TagsFilter: React.FC<TagsFilterProps> = ({ tags, onSelectTag }) => {
+//   const [selectedTagSlug, setSelectedTagSlug] = useState<string | null>(null);
+
+//   const handleTagClick = (slug: string) => {
+//     setSelectedTagSlug(slug);
+//     onSelectTag(slug);
+//   };
+
+//   const handleClearSelection = () => {
+//     setSelectedTagSlug(null);
+//     onSelectTag(null);
+//   };
+
+//   return (
+//     <div className="fixed text-center hidden md:block top-[6px] left-1/2 transform -translate-x-1/2 py-1 px-3 text-xs w-fit">
+//       Filter →
+//       {selectedTagSlug ? (
+//         <>
+//           <button
+//             onClick={handleClearSelection}
+//             className="ml-1 hover:text-maud-red"
+//           >
+//             All,
+//           </button>
+//           {tags
+//             .filter((tag) => tag.slug.current === selectedTagSlug)
+//             .map((tag) => (
+//               <button
+//                 key={tag._id}
+//                 onClick={handleClearSelection}
+//                 className="text-maud-red ml-1"
+//               >
+//                 {tag.title} x
+//               </button>
+//             ))}
+//         </>
+//       ) : (
+//         tags.map((tag) => (
+//           <button
+//             key={tag._id}
+//             onClick={() => handleTagClick(tag.slug.current)}
+//             className="ml-1 hover:text-maud-red"
+//           >
+//             {tag.title}
+//           </button>
+//         ))
+//       )}
+//     </div>
+//   );
+// };
+
+// export default TagsFilter;
+
 "use client";
 
 import React, { useState } from "react";
@@ -16,29 +79,49 @@ const TagsFilter: React.FC<TagsFilterProps> = ({ tags, onSelectTag }) => {
     onSelectTag(slug);
   };
 
+  const handleClearSelection = () => {
+    setSelectedTagSlug(null);
+    onSelectTag(null);
+  };
+
   return (
-    <div className="fixed hidden md:block top-[6px] left-1/2 transform -translate-x-1/2 py-1 px-3 text-xs w-fit">
+    <div className="fixed text-center hidden md:block top-[6px] left-1/2 transform -translate-x-1/2 py-1 px-3 text-xs w-fit">
       Filter →
-      {tags.map((tag) => (
-        <button
-          key={tag._id}
-          onClick={() => handleTagClick(tag.slug.current)}
-          className={` ml-1 hover:text-maud-red ${
-            selectedTagSlug === tag.slug.current ? "text-maud-red ml-1" : ""
-          }`}
-        >
-          {tag.title},
-        </button>
-      ))}
-      <button
-        onClick={() => {
-          setSelectedTagSlug(null);
-          onSelectTag(null);
-        }}
-        className="ml-1 hover:text-maud-red"
-      >
-        Clear
-      </button>
+      {selectedTagSlug ? (
+        <>
+          <button
+            onClick={handleClearSelection}
+            className="ml-1 hover:text-maud-red"
+          >
+            All,
+          </button>
+          {tags
+            .filter((tag) => tag.slug.current === selectedTagSlug)
+            .map((tag, index, filteredTags) => (
+              <React.Fragment key={tag._id}>
+                {index > 0 && ", "}
+                <button
+                  onClick={handleClearSelection}
+                  className="text-maud-red ml-1"
+                >
+                  {tag.title} ×
+                </button>
+              </React.Fragment>
+            ))}
+        </>
+      ) : (
+        tags.map((tag, index) => (
+          <React.Fragment key={tag._id}>
+            <button
+              onClick={() => handleTagClick(tag.slug.current)}
+              className="ml-1 hover:text-maud-red"
+            >
+              {tag.title}
+            </button>
+            {index < tags.length - 1 && ", "}
+          </React.Fragment>
+        ))
+      )}
     </div>
   );
 };
