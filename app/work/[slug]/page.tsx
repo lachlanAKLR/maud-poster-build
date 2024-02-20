7e2d51ea267f46d68e9c4b4dd4a637b793bd41ce
Lachy Richards <lachyrichards@Lachys-MacBook-Pro.local>
2024-02-20 13:58:33 +1100

    metadata

--- app/work/[slug]/page.tsx
+++ app/work/[slug]/page.tsx
@@ -3,6 +3,29 @@ import { loadQuery } from "@/sanity/lib/store";
 import { PROJECTS_QUERY, PROJECT_QUERY } from "@/sanity/lib/queries";
 import Project from "../../components/Project";
 import { client } from "@/sanity/lib/client";
+import { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: QueryParams;
+}): Promise<Metadata> {
+  const initial = await loadQuery<SanityDocument>(PROJECT_QUERY, params, {});
+
+  if (!initial)
+    return {
+      title: "Not Found",
+      description: "The page is not found",
+    };
+
+  return {
+    title: initial.data.title,
+    description: initial.data.subtitle,
+    openGraph: {
+      images: initial.data.featuredImage.image,
+    },
+  };
+}
 
 export async function generateStaticParams() {
   const projects = await client.fetch<SanityDocument[]>(PROJECTS_QUERY);
