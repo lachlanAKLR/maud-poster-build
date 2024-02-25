770820da7bebe2376af179d844a58d3c1987fddc
Lachy Richards <lachyrichards@Lachys-MacBook-Pro.local>
2024-02-26 10:04:27 +1100

    mobile timeout

--- app/components/UI/ScrollDown.tsx
+++ app/components/UI/ScrollDown.tsx
@@ -1,18 +1,50 @@
-// ScrollDown.tsx
+// // ScrollDown.tsx
+// "use client";
+
+// import { useEffect } from "react";
+
+// const ScrollDown = () => {
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       window.scrollBy({
+//         top: window.innerHeight, // Scrolls down one viewport height
+//         behavior: "smooth", // Mimics the smooth scroll of a mouse
+//       });
+//     }, 3000); // Executes after 3 seconds
+
+//     return () => clearTimeout(timer); // Cleanup the timeout
+//   }, []);
+
+//   return null; // This component does not render anything
+// };
+
+// export default ScrollDown;
+
 "use client";
 
 import { useEffect } from "react";
 
 const ScrollDown = () => {
   useEffect(() => {
-    const timer = setTimeout(() => {
-      window.scrollBy({
-        top: window.innerHeight, // Scrolls down one viewport height
-        behavior: "smooth", // Mimics the smooth scroll of a mouse
-      });
-    }, 3000); // Executes after 3 seconds
+    // Function to check if the current device is mobile
+    const isMobile = () => {
+      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      );
+    };
 
-    return () => clearTimeout(timer); // Cleanup the timeout
+    // Only set the timeout for scrolling if not on a mobile device
+    if (!isMobile()) {
+      const timer = setTimeout(() => {
+        window.scrollBy({
+          top: window.innerHeight, // Scrolls down one viewport height
+          behavior: "smooth", // Mimics the smooth scroll of a mouse
+        });
+      }, 3000); // Executes after 3 seconds
+
+      // Cleanup the timeout on component unmount
+      return () => clearTimeout(timer);
+    }
   }, []);
 
   return null; // This component does not render anything
