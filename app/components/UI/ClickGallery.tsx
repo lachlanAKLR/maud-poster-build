5df26a464a49c1415d2cf9321f540a7df0987d31
Lachy Richards <lachyrichards@Lachys-MacBook-Pro.local>
2024-03-06 13:43:07 +1100

    debug clikgallery

--- app/components/UI/ClickGallery.tsx
+++ app/components/UI/ClickGallery.tsx
@@ -1,158 +1,6 @@
-// "use client";
-
-// import React, { useState, useEffect } from "react";
-// import ArchiveThumb from "./ArchiveThumb";
-// import { SanityDocument } from "next-sanity";
-// import useMediaQuery from "../Utilities/useMediaQuery";
-// import { shuffleArray } from "../Utilities/shuffleArray";
-
-// interface ClickGalleryProps {
-//   documents: SanityDocument[];
-// }
-
-// interface DisplayedDocument {
-//   doc: SanityDocument;
-//   position: { x: number; y: number };
-//   width: number;
-// }
-
-// const ClickGallery: React.FC<ClickGalleryProps> = ({ documents }) => {
-//   const isSmallScreen = useMediaQuery("(max-width:768px)");
-//   const [shuffledDocuments, setShuffledDocuments] = useState<SanityDocument[]>(
-//     []
-//   );
-//   const [displayedDocuments, setDisplayedDocuments] = useState<
-//     DisplayedDocument[]
-//   >([]);
-//   const [currentPosition, setCurrentPosition] = useState<{
-//     x: number;
-//     y: number;
-//   } | null>(null);
-//   const [holdTimer, setHoldTimer] = useState<NodeJS.Timeout | null>(null);
-
-//   useEffect(() => {
-//     setShuffledDocuments(shuffleArray([...documents]));
-//   }, [documents]);
-
-//   const startHold = (x: number, y: number) => {
-//     const nextDocumentIndex =
-//       displayedDocuments.length % shuffledDocuments.length;
-//     const nextDocument = shuffledDocuments[nextDocumentIndex];
-//     const initialWidth = isSmallScreen ? 100 : 250;
-//     const initialPosition = {
-//       x: x - initialWidth / 2,
-//       y: y - initialWidth / 2,
-//     };
-//     const newDisplayDocument: DisplayedDocument = {
-//       doc: nextDocument,
-//       position: initialPosition,
-//       width: initialWidth,
-//     };
-
-//     setCurrentPosition(initialPosition);
-//     setDisplayedDocuments([...displayedDocuments, newDisplayDocument]);
-
-//     const timer = setInterval(() => {
-//       setDisplayedDocuments((currentDocs) =>
-//         currentDocs.map((doc, index) => {
-//           if (index === currentDocs.length - 1) {
-//             const widthIncrease = 2;
-
-//             return {
-//               ...doc,
-//               width: doc.width + widthIncrease,
-//               position: {
-//                 x: doc.position.x - widthIncrease / 2,
-//                 y: doc.position.y - widthIncrease / 2,
-//               },
-//             };
-//           }
-//           return doc;
-//         })
-//       );
-//     }, 0.25);
-
-//     setHoldTimer(timer);
-//   };
-
-//   const endHold = () => {
-//     if (holdTimer) {
-//       clearInterval(holdTimer);
-//       setHoldTimer(null);
-//     }
-//     setDisplayedDocuments((currentDocs) =>
-//       currentDocs.map((doc, index) => {
-//         if (index === currentDocs.length - 1) {
-//           return { ...doc };
-//         }
-//         return doc;
-//       })
-//     );
-//     setCurrentPosition(null);
-//   };
-
-//   // @ts-ignore
-//   const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
-//     startHold(event.clientX, event.clientY);
-//   };
-
-//   const handleTouchStart = (event: React.TouchEvent<HTMLDivElement>) => {
-//     const touch = event.touches[0];
-//     startHold(touch.clientX, touch.clientY);
-//   };
-
-//   const resetLayout = (event: React.MouseEvent<HTMLButtonElement>) => {
-//     event.stopPropagation(); // Prevents event from reaching the onMouseDown handler of the parent
-//     setDisplayedDocuments([]);
-//     setShuffledDocuments(shuffleArray([...documents])); // Reshuffle on reset
-//   };
-
-//   const handleContextMenu = (event: React.MouseEvent<HTMLDivElement>) => {
-//     event.preventDefault();
-//   };
-
-//   return (
-//     <>
-//       <div
-//         className="w-full h-dvh md:h-screen relative select-none"
-//         onMouseDown={handleMouseDown}
-//         onMouseUp={endHold}
-//         onMouseLeave={endHold}
-//         onTouchStart={handleTouchStart}
-//         onTouchEnd={endHold}
-//         onContextMenu={handleContextMenu}
-//         style={{ cursor: "pointer" }}
-//       >
-//         {displayedDocuments.map(({ doc, position, width }, index) => (
-//           <div
-//             key={index}
-//             style={{
-//               position: "absolute",
-//               left: position.x,
-//               top: position.y,
-//             }}
-//           >
-//             {/* @ts-ignore */}
-//             <ArchiveThumb data={doc} index={index} dynamicWidth={width} />
-//           </div>
-//         ))}
-//       </div>
-//       <button
-//         className="text-xs absolute bottom-0 right-0 text-white p-2 m-1 h-fit w-fit z-20"
-//         onClick={resetLayout}
-//       >
-//         Clear
-//       </button>
-//     </>
-//   );
-// };
-
-// export default ClickGallery;
-
 "use client";
 
 import React, { useState, useEffect } from "react";
-import ArchiveThumb from "./ArchiveThumb";
 import { SanityDocument } from "next-sanity";
 import useMediaQuery from "../Utilities/useMediaQuery";
 import { shuffleArray } from "../Utilities/shuffleArray";
@@ -169,6 +17,7 @@ interface DisplayedDocument {
 }
 
 const ClickGallery: React.FC<ClickGalleryProps> = ({ documents }) => {
+  const [isLongTouch, setIsLongTouch] = useState(false);
   const isSmallScreen = useMediaQuery("(max-width:768px)");
   const [shuffledDocuments, setShuffledDocuments] = useState<SanityDocument[]>(
     []
@@ -193,10 +42,6 @@ const ClickGallery: React.FC<ClickGalleryProps> = ({ documents }) => {
       if (document.archiveImage && document.archiveImage.image) {
         const img = new Image();
         img.src = document.archiveImage.image;
-        // Optionally, you can listen for the load event to know when each image is loaded
-        img.onload = () => {
-          console.log(`Image preloaded: ${img.src}`);
-        };
       }
     });
   };
@@ -269,10 +114,9 @@ const ClickGallery: React.FC<ClickGalleryProps> = ({ documents }) => {
   };
 
   const resetLayout = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.stopPropagation(); // Prevents event from reaching the onMouseDown handler of the parent
+    event.stopPropagation();
     setDisplayedDocuments([]);
-    setShuffledDocuments(shuffleArray([...documents])); // Reshuffle on reset
-    // Preload images again after resetting
+    setShuffledDocuments(shuffleArray([...documents]));
     preloadImages();
   };
 
@@ -280,15 +124,32 @@ const ClickGallery: React.FC<ClickGalleryProps> = ({ documents }) => {
     event.preventDefault();
   };
 
+  const endHoldMobile = () => {
+    if (holdTimer) {
+      clearInterval(holdTimer);
+      setHoldTimer(null);
+    }
+
+    setDisplayedDocuments((currentDocs) =>
+      currentDocs.map((doc, index) => {
+        if (index === currentDocs.length - 1) {
+          return { ...doc };
+        }
+        return doc;
+      })
+    );
+    setCurrentPosition(null);
+  };
+
   return (
     <>
       <div
         className="w-full h-dvh md:h-screen relative select-none"
-        onMouseDown={handleMouseDown}
-        onMouseUp={endHold}
+        onMouseDown={isSmallScreen ? undefined : handleMouseDown}
+        onMouseUp={isSmallScreen ? undefined : endHold}
         onMouseLeave={endHold}
-        onTouchStart={handleTouchStart}
-        onTouchEnd={endHold}
+        onTouchStart={isSmallScreen ? handleTouchStart : undefined}
+        onTouchEnd={isSmallScreen ? endHold : undefined}
         onContextMenu={handleContextMenu}
         style={{ cursor: "pointer" }}
       >
@@ -301,7 +162,6 @@ const ClickGallery: React.FC<ClickGalleryProps> = ({ documents }) => {
               top: position.y,
             }}
           >
-            {/* Assuming ArchiveThumb properly handles the passed "data" prop */}
             <ArchiveThumbImg data={doc} index={index} dynamicWidth={width} />
           </div>
         ))}
