c9d3e319d7cb0c519b3d2ab6519f27db1ba2d5e1
Lachy Richards <lachyrichards@Lachys-MBP.modem>
2024-04-26 11:22:29 +1000

    google

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,12 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Nav from "./components/UI/Nav";
 import { diatype } from "./fonts";
-import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "MAUD, Part of Accenture song",
   description: "A Brand Identity Company",
   metadataBase: new URL("https://maud-website.vercel.app"),
 };
-
-// test
 
 export default function RootLayout({
   children,
@@ -20,7 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${diatype.className} antialiased text-maud-black `}>
-        <GoogleAnalytics gaId="G-C8X92K9QE8" />
         <Nav />
         {children}
       </body>
