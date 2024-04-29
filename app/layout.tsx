14dd100687ff98dd7a4b29a45a6e545a815194c4
Lachy Richards <lachyrichards@Lachys-MacBook-Pro.local>
2024-04-29 10:01:50 +1000

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Nav from "./components/UI/Nav";
 import { diatype } from "./fonts";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "MAUD, Part of Accenture song",
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleAnalytics gaId="G-TYTT67R84C" />
       <body className={`${diatype.className} antialiased text-maud-black `}>
         <Nav />
         {children}
